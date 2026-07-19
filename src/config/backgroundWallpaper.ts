6ef26ca49d33b42a772bedf7c2a18248ce742760
michara233 <1196961433@qq.com>
2026-07-19 23:29:44 +0800

    Update backgroundWallpaper.ts

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -43,14 +43,14 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.webp",
+			"/assets/images/DesktopWallpaper/d1.webp",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.webp",
-			"assets/images/MobileWallpaper/m2.png",
-			"assets/images/MobileWallpaper/m3.jpg",
+			"/assets/images/MobileWallpaper/m1.webp",
+			"/assets/images/MobileWallpaper/m2.png",
+			"/assets/images/MobileWallpaper/m3.jpg",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组）
