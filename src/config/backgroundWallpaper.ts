3bd7913f62cecd9ded218fb9e9e62e0c1ade8090
michara233 <1196961433@qq.com>
2026-07-20 15:41:27 +0800

    修改背景为gif

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -44,6 +44,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
 			"assets/images/DesktopWallpaper/d1.webp",
+			"assets/images/DesktopWallpaper/1.gif",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
